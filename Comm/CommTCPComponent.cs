3e211cb04fba183869adfe83bb836b20cf1b051c
박병석 <stone75@daum.net>
2024-10-24 14:34:26 +0900

    TCP Comm. 변경작업

--- Comm/CommTCPComponent.cs
+++ Comm/CommTCPComponent.cs
@@ -1,10 +1,15 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices.WindowsRuntime;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Windows.Forms.VisualStyles;
+using System.Xml.Schema;
 using MCProtocol;
+using Raon.Lib.Plc;
 
 namespace HermleCS.Comm
 {
@@ -14,7 +19,8 @@ namespace HermleCS.Comm
         private int Port;
         private Mitsubishi.McFrame FrameType = Mitsubishi.McFrame.MC3E;
 
-        private string buffer;
+        // private string buffer;
+        private int[] buffer;
         private int errcode;
         private string errmsg;
 
@@ -23,6 +29,7 @@ namespace HermleCS.Comm
         private CommTCPComponent() { }
 
         private Mitsubishi.Plc plc = null;
+        // private Mitsubishi __melsec = null;
 
         public static CommTCPComponent Instance
         {
@@ -47,12 +54,12 @@ namespace HermleCS.Comm
                 {
                     // await plc.Open();
                     plc.Open();
-                    C.log("McProtocol Opened Success");
+                    C.log("PLC TCP Connection Open Success");
                }
             }
             catch (Exception ex)
             {
-                C.log("McProtocol Opened Failed : " + ex.ToString());
+                C.log("PLC TCP Connection Open Failed : " + ex.ToString());
                 return false;
             }
 
@@ -61,7 +68,7 @@ namespace HermleCS.Comm
 
         public bool destroy()
         {
-            C.log("McProtocol Destroy...");
+            C.log("PLC TCP Connection Destroy...");
 
             try
             {
@@ -69,15 +76,53 @@ namespace HermleCS.Comm
             }
             catch (Exception ex)
             {
-                C.log("McProtocol Close Exception : " + ex.ToString());
+                C.log("PLC TCP Connection Close Exception : " + ex.ToString());
                 return false;
             }
 
             return true;
         }
 
-        private async void readBlock(string deviceid, int length)
+        private async void readBlock(string addr, int length)
         {
+            // string addr = txtAddress.Text.ToString();
+            // string data = txtValue.Text.ToString();
+            // int[] value = new int[length];
+
+            if (buffer != null)
+            {
+                buffer = null;
+            }
+
+            buffer = new int[length];
+            try
+            {
+                if (plc.Connected == true)
+                {
+                    await plc?.ReadDeviceBlock($"D{addr}", length, buffer);
+
+                    // txtValue.Text = "";
+                    string readString = "";
+                    foreach (int v in buffer)
+                    {
+                        readString += v.ToString() + ",";
+                        C.log($"Read Value =  {v}");
+                    }
+                    C.log($"TCP Read Value {readString}");
+                    // txtValue.Text = readString;
+                }
+                else
+                {
+                    C.log("PLC Disconnected!!");
+                }
+                return;
+            }
+            catch (Exception ex)
+            {
+                C.log("PLC Read Block Exception : " + ex.ToString());
+            }
+
+            /*
             buffer = null;
             errcode = 0;
             int[] value = new int[length];
@@ -109,85 +154,101 @@ namespace HermleCS.Comm
 
                 C.log("McProtocol Close Exception : " + ex.ToString());
             }
+            */
+        }
+
+        private async void writeBlock(string addr, int[] block)
+        {
+            // buffer = null;
+            errcode = C.ERRNO_SUCCESS;
+            // int[] value = new int[length];
+
+            try
+            {
+                if (plc.Connected == true)
+                {
+                    await plc?.WriteDeviceBlock($"D{addr}", block.Length, block);
+                    // ReadDeviceBlock($"D{deviceid}", length, value);
+
+                    //foreach (int v in value)
+                    //{
+                    //    // buffer += v.ToString() + ",";
+                    //    buffer += v.ToString();
+                    //}
+                    string writeString = "";
+                    foreach (int v in block)
+                    {
+                        writeString += v.ToString() + ",";
+                        C.log($"Write Value =  {v}");
+                    }
+                    C.log($"TCP Write Value {writeString}");
+
+                    //Console.WriteLine($"WriteBlock Read Value = {buffer}");
+                }
+                else
+                {
+                    errcode = C.ERRNO_CONNECT;
+                    errmsg = "Write Block : PLC Disconnected";
+                    C.log("Write Block : PLC Disconnected!!");
+                }
+            }
+            catch (Exception ex)
+            {
+                errcode = C.ERRNO_EXCEPTION;
+                errmsg = ex.Message;
+                C.log(ex.ToString());
+            }
         }
 
         public override bool readMessage(string deviceid, int length, out string readVal)
         {
-            Console.WriteLine("readMessage..............");
+            C.log("PLC ReadMessage..............Start");
 
             try
             {
                 // retCode = plc.ReadDeviceRandom2(deviceid, 1, out rVal);
-                readVal = null;
+                // readVal = null;
                 readBlock(deviceid, length);
-                if (errcode == C.ERRNO_SUCCESS)
+
+                string readString = "";
+                foreach (int v in buffer)
                 {
-                    readVal = buffer;
+                    readString += v.ToString() + ",";
                 }
+                readVal = readString;
+
+                //if (errcode == C.ERRNO_SUCCESS)
+                //{
+                //    readVal = buffer;
+                //}
             }
             catch (Exception e)
             {
-                Console.WriteLine(e.Message);
-                readVal = "Err : " + e.Message;
+                C.log(e.Message);
+                readVal = "PLC ReadMessage Exception : " + e.Message;
                 return false;
             }
+
+            C.log("PLC ReadMessage..............End");
 
             return true;
         }
-        /*
-        private async void writeBlock(string deviceid, int length, )
-        {
-        buffer = null;
-        errcode = 0;
-        int[] value = new int[length];
 
-        try
+        public override bool sendMessage(string deviceid, int length, int[] val)
         {
-        if (plc.Connected == true)
-        {
-        await plc?.ReadDeviceBlock($"D{deviceid}", length, value);
-
-        foreach (int v in value)
-        {
-        // buffer += v.ToString() + ",";
-        buffer += v.ToString();
-        }
-        Console.WriteLine($"ReadBlock Read Value = {buffer}");
-        }
-        else
-        {
-        errcode = C.ERRNO_CONNECT;
-        errmsg = "PLC Disconnected";
-        Console.WriteLine("PLC Disconnected!!");
-        }
-
-        }
-        catch (Exception ex)
-        {
-        errcode = C.ERRNO_EXCEPTION;
-        errmsg = ex.Message;
-        Console.WriteLine(ex.ToString());
-        }
-        }
-        */
-    public override bool sendMessage(string deviceid, int length, int[] val)
-        {
-            /*
-            C.log("sendMessage..............");
-
-            int retCode;
+            C.log("PLC sendMessage..............Start");
 
             try
             {
-                retCode = plc.WriteDeviceRandom2(deviceid, 1, ref val);
+                writeBlock(deviceid, val);
             }
             catch (Exception e)
             {
-                Console.WriteLine(e.Message);
+                C.log(e.Message);
                 return false;
             }
-            */
 
+            C.log("PLC sendMessage..............End");
             return true;
         }
 
